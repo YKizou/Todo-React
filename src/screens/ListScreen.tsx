1829d20485662f3ceb815443d7aa2121875113af
YKizou <62426491+YKizou@users.noreply.github.com>
2021-12-05 22:35:51 +0100

    Add abiIty to add tasks

---
+++ src/screens/ListScreen.tsx
@@ -0,0 +1,32 @@
+import { nanoid } from "nanoid";
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+
+type Props = {}
+
+type Task = {
+    id : string;
+    label: string;
+}
+
+const ListScreen: React.FC<Props> = () => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [newTaskLabel, setNewTaskLabel] = useState('');
+    const handleNewTaskLabelChange = (e: ChangeEvent<HTMLInputElement>) => setNewTaskLabel(e.target.value)
+    const handleNewTaskKeyChange = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter" && newTaskLabel !== '') {
+            setTasks((tasks) => [...tasks, {id: nanoid(), label : newTaskLabel}]);
+            setNewTaskLabel("");
+        }
+    }; 
+    
+    return (
+        <div>
+            <ul>
+                {tasks.map((task) => (<li key={task.id}>{task.label}</li>))}
+            </ul>
+            <input value={newTaskLabel} onChange={handleNewTaskLabelChange} onKeyPress={handleNewTaskKeyChange} />
+            </div>
+    )
+}
+
+export default ListScreen
