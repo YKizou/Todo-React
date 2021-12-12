3be455cd0acc97336cf39fd1e3924af5ea78eb64
YKizou <62426491+YKizou@users.noreply.github.com>
2021-12-12 05:36:29 +0100

    Create a Focus View

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,27 @@ import {
 import ListScreen from "./screens/ListScreen";
 
 import { ReactDOM } from "react";
+import { NavLink } from "react-router-dom";
+import FocusScreen from "./screens/FocusScreen";
 
 function App() {
   return (
-    <div>
-      <ListScreen/>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <NavLink to="/">List</NavLink>
+        {' '}-{' '}
+        <NavLink to="/focus">Focus</NavLink>
+      </nav>
+      <br />
+
+      <Routes>
+        <Route path="/" element={<ListScreen/>}></Route>
+        <Route path="/focus" element={<FocusScreen/>}></Route>
+      </Routes>
+    </BrowserRouter>
+
+
+
   );
 }
 
---
+++ src/screens/FocusScreen.tsx
@@ -0,0 +1,16 @@
+import { nanoid } from "nanoid";
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import { resolveTripleslashReference } from "typescript";
+
+
+const FocusScreen: React.FC = () => {
+
+    return (
+        <div>
+          Focus View
+        </div>
+    )
+} 
+
+
+export default FocusScreen
